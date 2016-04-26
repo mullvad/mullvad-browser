cc07d227655069a9f2ecd5deaa50d94c5f534965
Arthur Edelstein <arthuredelstein@gmail.com>
2016-04-26 15:59:36 -0700

    Bug 18905: Hide unwanted items from help menu
    Bug 25660: Remove the "New Private Window" option

--- browser/base/content/browser-safebrowsing.js
+++ browser/base/content/browser-safebrowsing.js
@@ -7,6 +7,13 @@
 
 var gSafeBrowsing = {
   setReportPhishingMenu() {
+    // tor-browser#18905: hide these menu entries
+    if (
+      !Services.prefs.getBoolPref("browser.safebrowsing.phishing.enabled", true)
+    ) {
+      return;
+    }
+
     // In order to detect whether or not we're at the phishing warning
     // page, we have to check the documentURI instead of the currentURI.
     // This is because when the DocShell loads an error page, the
