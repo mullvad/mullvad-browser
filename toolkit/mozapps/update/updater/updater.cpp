aa7b017dac2f66522877c94c9e1b47d1e3c4ce92
Kathy Brade <brade@pearlcrescent.com>
2017-01-13 11:40:24 -0500

    Bug 4234: Use the Firefox Update Process for Base Browser.
    Windows: disable "runas" code path in updater (15201).
    Windows: avoid writing to the registry (16236).
    Also includes fixes for tickets 13047, 13301, 13356, 13594, 15406,
      16014, 16909, 24476, and 25909.
    
    Also fix bug 27221: purge the startup cache if the Base Browser
    version changed (even if the Firefox version and build ID did
    not change), e.g., after a minor Base Browser update.
    
    Also fix 32616: Disable GetSecureOutputDirectoryPath() functionality.
    
    Bug 26048: potentially confusing "restart to update" message
    
    Within the update doorhanger, remove the misleading message that mentions
    that windows will be restored after an update is applied, and replace the
    "Restart and Restore" button label with an existing
    "Restart to update Tor Browser" string.
    
    Bug 28885: notify users that update is downloading
    
    Add a "Downloading Base Browser update" item which appears in the
    hamburger (app) menu while the update service is downloading a MAR
    file. Before this change, the browser did not indicate to the user
    that an update was in progress, which is especially confusing in
    Tor Browser because downloads often take some time. If the user
    clicks on the new menu item, the about dialog is opened to allow
    the user to see download progress.
    
    As part of this fix, the update service was changed to always show
    update-related messages in the hamburger menu, even if the update
    was started in the foreground via the about dialog or via the
    "Check for Tor Browser Update" toolbar menu item. This change is
    consistent with the Tor Browser goal of making sure users are
    informed about the update process.
    
    Removed #28885 parts of this patch which have been uplifted to Firefox.

--- toolkit/mozapps/update/updater/updater.cpp
+++ toolkit/mozapps/update/updater/updater.cpp
@@ -78,8 +78,10 @@ void LaunchChild(int argc, const char** argv);
 void LaunchMacPostProcess(const char* aAppBundle);
 bool ObtainUpdaterArguments(int* aArgc, char*** aArgv,
                             MARChannelStringTable* aMARStrings);
+#  ifndef BASE_BROWSER_UPDATE
 bool ServeElevatedUpdate(int aArgc, const char** aArgv,
                          const char* aMARChannelID);
+#  endif
 void SetGroupOwnershipAndPermissions(const char* aAppBundle);
 bool PerformInstallationFromDMG(int argc, char** argv);
 struct UpdateServerThreadArgs {
@@ -872,6 +874,11 @@ static int ensure_copy_recursive(const NS_tchar* path, const NS_tchar* dest,
   if (S_ISLNK(sInfo.st_mode)) {
     return ensure_copy_symlink(path, dest);
   }
+
+  // Ignore Unix domain sockets. See #20691.
+  if (S_ISSOCK(sInfo.st_mode)) {
+    return 0;
+  }
 #endif
 
   if (!S_ISDIR(sInfo.st_mode)) {
@@ -967,7 +974,7 @@ static int rename_file(const NS_tchar* spath, const NS_tchar* dpath,
   return OK;
 }
 
-#ifdef XP_WIN
+#if defined(XP_WIN) && !defined(BASE_BROWSER_UPDATE)
 // Remove the directory pointed to by path and all of its files and
 // sub-directories. If a file is in use move it to the tobedeleted directory
 // and attempt to schedule removal of the file on reboot
@@ -1100,6 +1107,8 @@ static int backup_discard(const NS_tchar* path, const NS_tchar* relPath) {
            relBackup, relPath));
       return WRITE_ERROR_DELETE_BACKUP;
     }
+
+#  if !defined(BASE_BROWSER_UPDATE)
     // The MoveFileEx call to remove the file on OS reboot will fail if the
     // process doesn't have write access to the HKEY_LOCAL_MACHINE registry key
     // but this is ok since the installer / uninstaller will delete the
@@ -1116,6 +1125,7 @@ static int backup_discard(const NS_tchar* path, const NS_tchar* relPath) {
            "file: " LOG_S,
            relPath));
     }
+#  endif
   }
 #else
   if (rv) {
@@ -2630,7 +2640,9 @@ static int ProcessReplaceRequest() {
     if (NS_taccess(deleteDir, F_OK)) {
       NS_tmkdir(deleteDir, 0755);
     }
+#  if !defined(BASE_BROWSER_UPDATE)
     remove_recursive_on_reboot(tmpDir, deleteDir);
+#  endif
 #endif
   }
 
@@ -2745,8 +2757,14 @@ static void UpdateThreadFunc(void* param) {
     if (rv == OK) {
       rv = PopulategMARStrings();
       if (rv == OK) {
+#  ifdef BASE_BROWSER_VERSION_QUOTED
+        // Use the base browser version to prevent downgrade attacks.
+        const char* appVersion = BASE_BROWSER_VERSION_QUOTED;
+#  else
+        const char* appVersion = MOZ_APP_VERSION;
+#  endif
         rv = gArchiveReader.VerifyProductInformation(
-            gMARStrings.MARChannelID.get(), MOZ_APP_VERSION);
+            gMARStrings.MARChannelID.get(), appVersion);
       }
     }
 #endif
@@ -2850,12 +2868,16 @@ static void UpdateThreadFunc(void* param) {
 
 #ifdef XP_MACOSX
 static void ServeElevatedUpdateThreadFunc(void* param) {
+#  ifdef BASE_BROWSER_UPDATE
+  WriteStatusFile(ELEVATION_CANCELED);
+#  else
   UpdateServerThreadArgs* threadArgs = (UpdateServerThreadArgs*)param;
   gSucceeded = ServeElevatedUpdate(threadArgs->argc, threadArgs->argv,
                                    threadArgs->marChannelID);
   if (!gSucceeded) {
     WriteStatusFile(ELEVATION_CANCELED);
   }
+#  endif
   QuitProgressUI();
 }
 
@@ -3022,8 +3044,12 @@ int NS_main(int argc, NS_tchar** argv) {
   // This will be used to set `gIsElevated`, but we are going to do it later
   // when we are ready to set it for every OS to avoid inconsistency.
   bool isElevated =
+#  ifdef BASE_BROWSER_UPDATE
+      false;
+#  else
       strstr(argv[0], "/Library/PrivilegedHelperTools/org.mozilla.updater") !=
       0;
+#  endif
   if (isElevated) {
     if (!ObtainUpdaterArguments(&argc, &argv, &gMARStrings)) {
       // Won't actually get here because ObtainUpdaterArguments will terminate
@@ -3802,6 +3828,14 @@ int NS_main(int argc, NS_tchar** argv) {
         if (!useService && !noServiceFallback &&
             (updateLockFileHandle == INVALID_HANDLE_VALUE ||
              forceServiceFallback)) {
+#  ifdef BASE_BROWSER_UPDATE
+          // To avoid potential security issues such as CVE-2015-0833, do not
+          // attempt to elevate privileges. Instead, write a "failed" message to
+          // the update status file (this function will return immediately after
+          // the CloseHandle(elevatedFileHandle) call below).
+          LOG(("Refusing to elevate in Base Browser."));
+          WriteStatusFile(WRITE_ERROR_ACCESS_DENIED);
+#  else
           LOG(("Elevating via a UAC prompt"));
           // Get the secure ID before trying to update so it is possible to
           // determine if the updater has created a new one.
@@ -3875,6 +3909,7 @@ int NS_main(int argc, NS_tchar** argv) {
             WriteStatusFile(ELEVATION_CANCELED);
             LOG(("Elevation canceled."));
           }
+#  endif /* BASE_BROWSER_UPDATE */
         } else {
           LOG(("Not showing a UAC prompt."));
           LOG(("useService=%s", useService ? "true" : "false"));
@@ -4258,6 +4293,7 @@ int NS_main(int argc, NS_tchar** argv) {
     if (!sStagedUpdate && !sReplaceRequest && _wrmdir(gDeleteDirPath)) {
       LOG(("NS_main: unable to remove directory: " LOG_S ", err: %d",
            DELETE_DIR, errno));
+#  if !defined(BASE_BROWSER_UPDATE)
       // The directory probably couldn't be removed due to it containing files
       // that are in use and will be removed on OS reboot. The call to remove
       // the directory on OS reboot is done after the calls to remove the files
@@ -4277,6 +4313,7 @@ int NS_main(int argc, NS_tchar** argv) {
              "directory: " LOG_S,
              DELETE_DIR));
       }
+#  endif /* BASE_BROWSER_UPDATE */
     }
 #endif /* XP_WIN */
 
