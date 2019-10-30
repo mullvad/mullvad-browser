43e1557459426504dbef88d927baf3ff582ee061
Alex Catarineu <acat@torproject.org>
2019-10-30 10:44:48 +0100

    BB 27604: Fix addon issues when moving the profile directory
    Bugzilla: https://bugzilla.mozilla.org/show_bug.cgi?id=1429838

--- toolkit/mozapps/extensions/internal/XPIProvider.sys.mjs
+++ toolkit/mozapps/extensions/internal/XPIProvider.sys.mjs
@@ -498,7 +498,7 @@ class XPIState {
 
     // Builds prior to be 1512436 did not include the rootURI property.
     // If we're updating from such a build, add that property now.
-    if (!("rootURI" in this) && this.file) {
+    if (this.file) {
       this.rootURI = getURIForResourceInFile(this.file, "").spec;
     }
 
@@ -511,7 +511,10 @@ class XPIState {
       saved.currentModifiedTime != this.lastModifiedTime
     ) {
       this.lastModifiedTime = saved.currentModifiedTime;
-    } else if (saved.currentModifiedTime === null) {
+    } else if (
+      saved.currentModifiedTime === null &&
+      (!this.file || !this.file.exists())
+    ) {
       this.missing = true;
     }
   }
@@ -1616,6 +1619,7 @@ var XPIStates = {
 
       if (shouldRestoreLocationData && oldState[loc.name]) {
         loc.restore(oldState[loc.name]);
+        changed = changed || loc.path != oldState[loc.name].path;
       }
       changed = changed || loc.changed;
 
