4111159f3cbda9cd403881522798eb93199b662e
Kathy Brade <brade@pearlcrescent.com>
2020-07-14 11:15:07 -0400

    BB 33852: Clean up about:logins (LockWise) to avoid mentioning sync, etc.
    Hide elements on about:logins that mention sync, "Firefox LockWise", and
    Mozilla's LockWise mobile apps.
    
    Disable the "Create New Login" button when security.nocertdb is true.

--- browser/components/aboutlogins/AboutLoginsParent.sys.mjs
+++ browser/components/aboutlogins/AboutLoginsParent.sys.mjs
@@ -49,6 +49,7 @@ ChromeUtils.defineLazyGetter(lazy, "AboutLoginsL10n", () => {
 const ABOUT_LOGINS_ORIGIN = "about:logins";
 const AUTH_TIMEOUT_MS = 5 * 60 * 1000; // 5 minutes
 const PRIMARY_PASSWORD_NOTIFICATION_ID = "primary-password-login-required";
+const NOCERTDB_PREF = "security.nocertdb";
 
 // about:logins will always use the privileged content process,
 // even if it is disabled for other consumers such as about:newtab.
@@ -324,6 +325,7 @@ export class AboutLoginsParent extends JSWindowActorParent {
           Services.policies.isAllowed("profileImport") &&
           AppConstants.platform != "linux",
         preselectedLogin: this.preselectedLogin,
+        canCreateLogins: !Services.prefs.getBoolPref(NOCERTDB_PREF, false),
       });
 
       await AboutLogins.sendAllLoginRelatedObjects(
