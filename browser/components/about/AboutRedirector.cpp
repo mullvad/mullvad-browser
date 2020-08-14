67de62b0941a4ca8170f6b5cec9ab53935820c63
Kathy Brade <brade@pearlcrescent.com>
2020-08-14 09:06:33 -0400

    Bug 40002: Remove about:ion
    Firefox Ion (previously Firefox Pioneer) is an opt-in program in which people
    volunteer to participate in studies that collect detailed, sensitive data about
    how they use their browser.
    
    Bug 41662: Disable about:sync-logs
    
    Even though we disable sync by default with
    `identity.fxaccounts.enabled`, this about: page is still avilable.
    We could throw an exception on the constructor of the related
    component, but it would result only in an error in the console, without
    a visible "this address does not look right" error page.
    If we fix the issues with MOZ_SERVICES_SYNC, we can restore the
    component.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -151,9 +151,6 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_MUST_LOAD_IN_CHILD | nsIAboutModule::ALLOW_SCRIPT |
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::IS_SECURE_CHROME_UI},
-    {"ion", "chrome://browser/content/ion.html",
-     nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::HIDE_FROM_ABOUTABOUT |
-         nsIAboutModule::IS_SECURE_CHROME_UI},
 };
 
 static nsAutoCString GetAboutModuleName(nsIURI* aURI) {
