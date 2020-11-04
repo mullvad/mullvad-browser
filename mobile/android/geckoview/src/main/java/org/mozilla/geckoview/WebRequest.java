575a2254b07fb09c9f32e2605d5c0c288a490085
Alex Catarineu <acat@torproject.org>
2020-11-04 15:58:22 +0100

    Bug 40171: Make WebRequest and GeckoWebExecutor First-Party aware

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/WebRequest.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/WebRequest.java
@@ -49,6 +49,11 @@ public class WebRequest extends WebMessage {
   /** The value of the Referer header for this request. */
   public final @Nullable String referrer;
 
+  /**
+   * The value of the origin of this request.
+   */
+  public final @Nullable String origin;
+
   @Retention(RetentionPolicy.SOURCE)
   @IntDef({
     CACHE_MODE_DEFAULT,
@@ -110,6 +115,7 @@ public class WebRequest extends WebMessage {
     cacheMode = builder.mCacheMode;
     referrer = builder.mReferrer;
     beConservative = builder.mBeConservative;
+    origin = builder.mOrigin;
 
     if (builder.mBody != null) {
       body = builder.mBody.asReadOnlyBuffer();
@@ -125,6 +131,7 @@ public class WebRequest extends WebMessage {
     /* package */ int mCacheMode = CACHE_MODE_DEFAULT;
     /* package */ String mReferrer;
     /* package */ boolean mBeConservative;
+    /* package */ String mOrigin;
 
     /**
      * Construct a Builder instance with the specified URI.
@@ -234,6 +241,17 @@ public class WebRequest extends WebMessage {
       mBeConservative = beConservative;
       return this;
     }
+
+    /**
+     * Set the origin URI.
+     *
+     * @param origin A URI String
+     * @return This Builder instance.
+     */
+     public @NonNull Builder origin(final @Nullable String origin) {
+       mOrigin = origin;
+       return this;
+     }
 
     /**
      * @return A {@link WebRequest} constructed with the values from this Builder instance.
