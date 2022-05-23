85da1d939706028bca693688d7ee7ecf31c3955d
Pier Angelo Vendrame <pierov@torproject.org>
2022-05-23 17:01:24 +0200

    Base Browser's .mozconfigs.
    Bug 17858: Cannot create incremental MARs for hardened builds.
    Define HOST_CFLAGS, etc. to avoid compiling programs such as mbsdiff
    (which is part of mar-tools and is not distributed to end-users) with
    ASan.
    
    Bug 21849: Don't allow SSL key logging.
    
    Bug 25741 - TBA: Disable features at compile-time
    
    Define MOZ_ANDROID_NETWORK_STATE and MOZ_ANDROID_LOCATION
    
    Bug 27623 - Export MOZILLA_OFFICIAL during desktop builds
    
    This fixes a problem where some preferences had the wrong default value.
    Also see bug 27472 where we made a similar fix for Android.
    
    Bug 29859: Disable HLS support for now
    
    Bug 30463: Explicitly disable MOZ_TELEMETRY_REPORTING
    
    Bug 32493: Disable MOZ_SERVICES_HEALTHREPORT
    
    Bug 33734: Set MOZ_NORMANDY to False
    
    Bug 33851: Omit Parental Controls.
    
    Bug 40252: Add --enable-rust-simd to our tor-browser mozconfig files
    
    Bug 41584: Move some configuration options to base-browser level

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -5,26 +5,6 @@
 
 MOZ_APP_VENDOR=Mozilla
 
-if test "$OS_ARCH" = "WINNT"; then
-  if ! test "$HAVE_64BIT_BUILD"; then
-    if test "$MOZ_UPDATE_CHANNEL" = "nightly" -o \
-            "$MOZ_UPDATE_CHANNEL" = "nightly-try" -o \
-            "$MOZ_UPDATE_CHANNEL" = "aurora" -o \
-            "$MOZ_UPDATE_CHANNEL" = "beta" -o \
-            "$MOZ_UPDATE_CHANNEL" = "release"; then
-      if ! test "$MOZ_DEBUG"; then
-        if ! test "$USE_STUB_INSTALLER"; then
-          # Expect USE_STUB_INSTALLER from taskcluster for downstream task consistency
-          echo "ERROR: STUB installer expected to be enabled but"
-          echo "ERROR: USE_STUB_INSTALLER is not specified in the environment"
-          exit 1
-        fi
-        MOZ_STUB_INSTALLER=1
-      fi
-    fi
-  fi
-fi
-
 BROWSER_CHROME_URL=chrome://browser/content/browser.xhtml
 
 # MOZ_APP_DISPLAYNAME will be set by branding/configure.sh
@@ -37,7 +17,8 @@ MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 
-MOZ_PROFILE_MIGRATOR=1
+# tor-browser#41577: Do not enable profile migration
+# MOZ_PROFILE_MIGRATOR=1
 
 # Include the DevTools client, not just the server (which is the default)
 MOZ_DEVTOOLS=all
