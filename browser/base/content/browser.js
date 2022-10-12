5b2779a52eed7ab0b4ae27e75a97928f6f9c855b
Dan Ballard <dan@mindstab.net>
2022-10-12 12:54:59 -0700

    Bug 41117: using MOZ_PROXY_BYPASS_PROTECTION disable share menu on mac and windows, and restore email link on mac

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -4807,6 +4807,10 @@ let gShareUtils = {
    * Updates a sharing item in a given menu, creating it if necessary.
    */
   updateShareURLMenuItem(browser, insertAfterEl) {
+    if (!Services.prefs.getBoolPref("browser.share_menu.allow", false)) {
+      return false;
+    }
+
     // We only support "share URL" on macOS and on Windows 10:
     if (
       AppConstants.platform != "macosx" &&
