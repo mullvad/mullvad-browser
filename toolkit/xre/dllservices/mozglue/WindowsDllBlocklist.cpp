24ff518db305c19251301608c7f62dd4ae667563
Dan Ballard <dan@mindstab.net>
2022-10-21 11:39:58 -0700

    Bug 41149: Re-enable DLL injection protection in all builds not just nightlies

--- toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
+++ toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
@@ -576,7 +576,7 @@ continue_loading:
   return ret;
 }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER_VERSION)
 // Map of specific thread proc addresses we should block. In particular,
 // LoadLibrary* APIs which indicate DLL injection
 static void* gStartAddressesToBlock[4];
@@ -589,7 +589,7 @@ static bool ShouldBlockThread(void* aStartAddress) {
     return false;
   }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER_VERSION)
   for (auto p : gStartAddressesToBlock) {
     if (p == aStartAddress) {
       return true;
@@ -662,7 +662,7 @@ MFBT_API void DllBlocklist_Initialize(uint32_t aInitFlags) {
     }
   }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER_VERSION)
   // Populate a list of thread start addresses to block.
   HMODULE hKernel = GetModuleHandleW(L"kernel32.dll");
   if (hKernel) {
