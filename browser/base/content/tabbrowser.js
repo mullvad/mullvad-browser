6850b28cfe6a29b562b2553d3e531b006acf095e
hackademix <giorgio@maone.net>
2022-12-11 13:28:57 +0100

    Bug 32308: Use direct browser sizing for letterboxing.
    Bug 30556: align letterboxing with 200x100 new win width stepping

--- browser/base/content/tabbrowser.js
+++ browser/base/content/tabbrowser.js
@@ -2226,6 +2226,10 @@
       stack.className = "browserStack";
       stack.appendChild(b);
 
+      let decorator = document.createXULElement("hbox");
+      decorator.className = "browserDecorator";
+      stack.appendChild(decorator);
+
       let browserContainer = document.createXULElement("vbox");
       browserContainer.className = "browserContainer";
       browserContainer.appendChild(notificationbox);
