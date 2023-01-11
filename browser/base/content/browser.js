a9e25a107cdaf5fdf203b208b628a3c994b4c1a0
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2023-01-11 08:37:43 +0000

    Bug 41565: Gate Telemetry Tasks behind MOZ_TELEMETRY_REPORTING
    (cherry picked from commit 47eb7d305746bd1a9e328b1fb7fef40d56769d5b)

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -6004,6 +6004,7 @@ var TabsProgressListener = {
   onStateChange(aBrowser, aWebProgress, aRequest, aStateFlags, aStatus) {
     // Collect telemetry data about tab load times.
     if (
+      AppConstants.MOZ_TELEMETRY_REPORTING &&
       aWebProgress.isTopLevel &&
       (!aRequest.originalURI || aRequest.originalURI.scheme != "about")
     ) {
