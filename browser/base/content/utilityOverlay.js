3cd00fbe5ac7d0925373eef253939238233efa70
Henry Wilkes <henry@torproject.org>
2023-02-13 13:47:12 +0000

    MB 39: Add home page about:mullvad-browser

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -78,6 +78,7 @@ function isBlankPageURL(aURL) {
   return (
     aURL == "about:blank" ||
     aURL == "about:home" ||
+    aURL == "about:mullvad-browser" ||
     aURL == BROWSER_NEW_TAB_URL ||
     aURL == "chrome://browser/content/blanktab.html"
   );
