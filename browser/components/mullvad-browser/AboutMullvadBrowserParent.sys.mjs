161a6b7a2bd9c254ce3e12c2bf1efccf551c7e16
Henry Wilkes <henry@torproject.org>
2023-02-13 13:47:12 +0000

    MB 39: Add home page about:mullvad-browser

---
+++ browser/components/mullvad-browser/AboutMullvadBrowserParent.sys.mjs
@@ -0,0 +1,26 @@
+/**
+ * Actor parent class for the about:mullvad-browser page.
+ */
+export class AboutMullvadBrowserParent extends JSWindowActorParent {
+  receiveMessage(message) {
+    const shouldNotifyPref = "mullvadbrowser.post_update.shouldNotify";
+    switch (message.name) {
+      case "AboutMullvadBrowser:GetUpdateData":
+        if (!Services.prefs.getBoolPref(shouldNotifyPref, false)) {
+          return Promise.resolve(null);
+        }
+        Services.prefs.clearUserPref(shouldNotifyPref);
+        return Promise.resolve({
+          version: Services.prefs.getCharPref(
+            "browser.startup.homepage_override.mullvadbrowser.version"
+          ),
+          url:
+            Services.prefs.getCharPref("mullvadbrowser.post_update.url", "") ||
+            Services.urlFormatter.formatURLPref(
+              "startup.homepage_override_url"
+            ),
+        });
+    }
+    return undefined;
+  }
+}
