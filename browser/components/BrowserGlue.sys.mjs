9d7710264fdea7b50d60674c2b5c570ccdeba0a8
Pier Angelo Vendrame <pierov@torproject.org>
2023-02-16 16:59:41 +0100

    Bug 41635: Disable the Normandy component
    Do not include Normandy at all whenever MOZ_NORMANDY is False.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -2205,7 +2205,11 @@ BrowserGlue.prototype = {
       () => lazy.PageDataService.uninit(),
       () => lazy.PageThumbs.uninit(),
       () => lazy.NewTabUtils.uninit(),
-      () => lazy.Normandy.uninit(),
+      () => {
+        if (AppConstants.MOZ_NORMANDY) {
+          lazy.Normandy.uninit();
+        }
+      },
       () => lazy.RFPHelper.uninit(),
       () => lazy.ShoppingUtils.uninit(),
       () => lazy.ASRouterNewTabHook.destroy(),
