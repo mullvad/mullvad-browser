1f38790237a15df07d1f53eec08b0799f7b9f980
Pier Angelo Vendrame <pierov@torproject.org>
2023-02-20 16:34:33 +0100

    Bug 41599: Always return an empty string as network ID
    Firefox computes an internal network ID used to detect network changes
    and act consequently (e.g., to improve WebSocket UX).
    However, there are a few ways to get this internal network ID, so we
    patch them out, to be sure any new code will not be able to use them and
    possibly link users.
    
    We also sent a patch to Mozilla to seed the internal network ID, to
    prevent any accidental leak in the future.
    Upstream: https://bugzilla.mozilla.org/show_bug.cgi?id=1817756

--- netwerk/system/netlink/NetlinkService.cpp
+++ netwerk/system/netlink/NetlinkService.cpp
@@ -1871,8 +1871,12 @@ void NetlinkService::CalculateNetworkID() {
 }
 
 void NetlinkService::GetNetworkID(nsACString& aNetworkID) {
+#ifdef BASE_BROWSER_VERSION
+  aNetworkID.Truncate();
+#else
   MutexAutoLock lock(mMutex);
   aNetworkID = mNetworkId;
+#endif
 }
 
 nsresult NetlinkService::GetDnsSuffixList(nsTArray<nsCString>& aDnsSuffixList) {
