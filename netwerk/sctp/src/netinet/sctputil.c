b542a37ef48af5a4ad81c0acf7df86a1e1e51dba
Marco Simonelli <msimonelli@protonmail.com>
2023-03-10 11:59:37 +0000

    Bug 41459: WebRTC fails to build under mingw (Part 4)
    - fixes requried to build netwerk/sctp

--- netwerk/sctp/src/netinet/sctputil.c
+++ netwerk/sctp/src/netinet/sctputil.c
@@ -82,6 +82,10 @@ __FBSDID("$FreeBSD$");
 #endif
 #endif
 
+#if defined(_WIN32) && !defined(_MSC_VER)
+#include <minmax.h>
+#endif
+
 extern const struct sctp_cc_functions sctp_cc_functions[];
 extern const struct sctp_ss_functions sctp_ss_functions[];
 
