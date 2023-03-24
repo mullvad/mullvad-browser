c3baeaa237784940f36549f8064f03cb6cce6521
hackademix <giorgio@maone.net>
2023-03-24 19:40:19 +0100

    Bug 41695: Warn on window maximization without letterboxing in RFPHelper module

--- browser/app/profile/001-base-profile.js
+++ browser/app/profile/001-base-profile.js
@@ -382,6 +382,8 @@ pref("privacy.resistFingerprinting.letterboxing.vcenter", true);
 pref("privacy.resistFingerprinting.letterboxing.gradient", true);
 // tor-browser#41918: should we reuse last window sizes if letterboxing is enabled
 pref("privacy.resistFingerprinting.letterboxing.rememberSize", false);
+// tor-browser#41695: how many warnings we show if user closes them without restoring the window size
+pref("privacy.resistFingerprinting.resizeWarnings", 3);
 // tor-browser#33282: new windows start at 1400x900 when there's enough screen space, otherwise down by 200x100 blocks
 pref("privacy.window.maxInnerWidth", 1400);
 pref("privacy.window.maxInnerHeight", 900);
