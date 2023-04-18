f2d14ab4d5b0293ba195949aa96c9214f62590c3
Pier Angelo Vendrame <pierov@torproject.org>
2023-04-18 16:41:40 +0200

    MB 160: Disable the cookie exceptions button
    Besides disabling the "Delete on close checkbox", disable also the
    "Manage Exceptions" button when always using PBM.

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -1299,6 +1299,7 @@ var gPrivacyPane = {
     let blockCookiesMenu = document.getElementById("blockCookiesMenu");
     let deleteOnCloseCheckbox = document.getElementById("deleteOnClose");
     let deleteOnCloseNote = document.getElementById("deleteOnCloseNote");
+    let cookieExceptions = document.getElementById("cookieExceptions");
     let blockCookies = behavior != Ci.nsICookieService.BEHAVIOR_ACCEPT;
     let cookieBehaviorLocked = Services.prefs.prefIsLocked(
       "network.cookie.cookieBehavior"
@@ -1316,6 +1317,7 @@ var gPrivacyPane = {
     deleteOnCloseCheckbox.disabled =
       privateBrowsing || completelyBlockCookies || cookieExpirationLocked;
     deleteOnCloseNote.hidden = !privateBrowsing;
+    cookieExceptions.disabled = privateBrowsing;
 
     switch (behavior) {
       case Ci.nsICookieService.BEHAVIOR_ACCEPT:
