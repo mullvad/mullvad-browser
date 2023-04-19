66fde012aa50b017ac101ccc0be6546ce4d26d10
Henry Wilkes <henry@torproject.org>
2023-04-19 14:22:18 +0100

    Bug 41736: Customize toolbar for base-browser.

--- browser/components/extensions/parent/ext-browserAction.js
+++ browser/components/extensions/parent/ext-browserAction.js
@@ -193,6 +193,10 @@ this.browserAction = class extends ExtensionAPIPersistent {
   }
 
   build() {
+    // The extension ID for NoScript (WebExtension)
+    const isNoScript =
+      this.extension.id === "{73a6fe31-595d-460b-a920-fcc0f8843232}";
+
     let widget = CustomizableUI.createWidget({
       id: this.id,
       viewId: this.viewId,
@@ -200,7 +204,11 @@ this.browserAction = class extends ExtensionAPIPersistent {
       removable: true,
       label: this.action.getProperty(null, "title"),
       tooltiptext: this.action.getProperty(null, "title"),
-      defaultArea: browserAreas[this.action.getDefaultArea()],
+      // Do not want to add the NoScript extension to the toolbar by default.
+      // tor-browser#41736
+      defaultArea: isNoScript
+        ? null
+        : browserAreas[this.action.getDefaultArea()],
       showInPrivateBrowsing: this.extension.privateBrowsingAllowed,
 
       // Don't attempt to load properties from the built-in widget string
