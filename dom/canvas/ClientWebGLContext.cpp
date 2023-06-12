3ddf3a219fc77b8abd476f5043c29c7cf9679c09
Pier Angelo Vendrame <pierov@torproject.org>
2023-06-12 16:54:33 +0200

    fixup! Bug 30541: Disable WebGL readPixel() for web content
    ShouldResistFinterprinting now always wants a target.

--- dom/canvas/ClientWebGLContext.cpp
+++ dom/canvas/ClientWebGLContext.cpp
@@ -5135,8 +5135,7 @@ bool ClientWebGLContext::ReadPixels_SharedPrecheck(
 
   // Security check passed, but don't let content readPixel calls through for
   // now, if Resist Fingerprinting Mode is enabled.
-  if (nsContentUtils::ShouldResistFingerprinting(aCallerType,
-                                                 GetParentObject())) {
+  if (ShouldResistFingerprinting()) {
     JsWarning("readPixels: Not allowed in Resist Fingerprinting Mode");
     out_error.Throw(NS_ERROR_DOM_NOT_SUPPORTED_ERR);
     return false;
