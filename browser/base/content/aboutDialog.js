9cb8573b394880d64b16e885b8cc0ad4679219e1
Pier Angelo Vendrame <pierov@torproject.org>
2023-08-29 18:34:00 +0200

    fixup! Bug 4234: Use the Firefox Update Process for Base Browser.
    Bug 41528: Localized the version string in the about dialog

--- browser/base/content/aboutDialog.js
+++ browser/base/content/aboutDialog.js
@@ -43,18 +43,11 @@ function init() {
   }
 
   // Include the build ID and display warning if this is an "a#" (nightly or aurora) build
-  let versionId = "aboutDialog-version";
+  let versionId = "basebrowser-about-dialog-version";
   let versionAttributes = {
-    version: AppConstants.MOZ_APP_VERSION_DISPLAY,
-    bits: Services.appinfo.is64Bit ? 64 : 32,
+    version: AppConstants.BASE_BROWSER_VERSION,
+    firefoxVersion: AppConstants.MOZ_APP_VERSION_DISPLAY,
   };
-
-  // Adjust version text to show the fork version
-  versionAttributes.version =
-    AppConstants.BASE_BROWSER_VERSION +
-    " (based on Mozilla Firefox " +
-    AppConstants.MOZ_APP_VERSION_DISPLAY +
-    ")";
 
   let version = Services.appinfo.version;
   if (/a\d+$/.test(version)) {
