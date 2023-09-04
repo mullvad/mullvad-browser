9d859dd4c1433f68dcf4aa264e168dad4ae82e2f
Pier Angelo Vendrame <pierov@torproject.org>
2023-09-04 15:58:31 +0200

    fixup! Bug 9173: Change the default Firefox profile directory to be relative.
    Add a method to query whether the browser is in portable mode.

--- toolkit/xre/nsXREDirProvider.cpp
+++ toolkit/xre/nsXREDirProvider.cpp
@@ -1287,6 +1287,20 @@ nsresult nsXREDirProvider::GetPortableDataDir(nsIFile** aFile,
 }
 #endif
 
+NS_IMETHODIMP nsXREDirProvider::GetIsPortableMode(bool* aIsPortableMode) {
+#ifdef RELATIVE_DATA_DIR
+  if (gDataDirPortable) {
+    *aIsPortableMode = *gDataDirPortable;
+  } else {
+    nsCOMPtr<nsIFile> dir;
+    GetPortableDataDir(getter_AddRefs(dir), *aIsPortableMode);
+  }
+#else
+  *aIsPortableMode = false;
+#endif
+  return NS_OK;
+}
+
 nsresult nsXREDirProvider::GetUserDataDirectoryHome(nsIFile** aFile,
                                                     bool aLocal) {
   // Copied from nsAppFileLocationProvider (more or less)
