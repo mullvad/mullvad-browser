97184c529acfa77b45516afcea2167faac09d9e4
Mark Banner <standard8@mozilla.com>
2023-09-08 11:26:54 +0000

    Bug 1845752. r=ckerschb
    Differential Revision: https://phabricator.services.mozilla.com/D186676

--- toolkit/components/search/SearchEngine.sys.mjs
+++ toolkit/components/search/SearchEngine.sys.mjs
@@ -821,7 +821,10 @@ export class SearchEngine {
           this._hasPreferredIcon = isPreferred;
         };
 
-        let chan = lazy.SearchUtils.makeChannel(uri);
+        let chan = lazy.SearchUtils.makeChannel(
+          uri,
+          Ci.nsIContentPolicy.TYPE_IMAGE
+        );
         let listener = new lazy.SearchUtils.LoadListener(
           chan,
           /^image\//,
