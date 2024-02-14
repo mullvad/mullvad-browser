587a2073c1918dd31e58bdcaee4390c2b0b393ce
Pier Angelo Vendrame <pierov@torproject.org>
2024-02-14 17:08:34 +0100

    MB 80: Enable Mullvad Browser as a default browser

--- toolkit/mozapps/defaultagent/EventLog.h
+++ toolkit/mozapps/defaultagent/EventLog.h
@@ -17,8 +17,8 @@ MOZ_END_EXTERN_C
 
 #include "mozilla/WindowsEventLog.h"
 
-#define LOG_ERROR(hr) MOZ_WIN_EVENT_LOG_ERROR(gWinEventLogSourceName, hr)
-#define LOG_ERROR_MESSAGE(format, ...) \
-  MOZ_WIN_EVENT_LOG_ERROR_MESSAGE(gWinEventLogSourceName, format, __VA_ARGS__)
+// This is not compatible with Mingw.
+#define LOG_ERROR(hr)
+#define LOG_ERROR_MESSAGE(format, ...)
 
 #endif  // __DEFAULT_BROWSER_AGENT_EVENT_LOG_H__
