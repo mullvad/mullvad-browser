695f1a5e7eff18f9f73f5662e1ed449aebc8f7e3
hackademix <giorgio@maone.net>
2024-03-06 22:00:01 +0100

    Bug 41916: Letterboxing preferences UI

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -758,6 +758,7 @@ var gMainPane = {
     ]);
 
     AppearanceChooser.init();
+    gLetterboxingPrefs.init();
 
     // Notify observers that the UI is now ready
     Services.obs.notifyObservers(window, "main-pane-loaded");
@@ -2613,6 +2614,7 @@ var gMainPane = {
     Services.prefs.removeObserver(PREF_CONTAINERS_EXTENSION, this);
     Services.obs.removeObserver(this, AUTO_UPDATE_CHANGED_TOPIC);
     Services.obs.removeObserver(this, BACKGROUND_UPDATE_CHANGED_TOPIC);
+    gLetterboxingPrefs.destroy();
     AppearanceChooser.destroy();
   },
 
