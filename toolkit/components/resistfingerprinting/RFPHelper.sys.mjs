c9804e100bf8a483f06a60cfe359e2a9cb632c42
hackademix <giorgio@maone.net>
2024-03-10 15:01:07 +0100

    BB 42443: Shrink window to match letterboxing size when the emtpy area is clicked.

--- toolkit/components/resistfingerprinting/RFPHelper.sys.mjs
+++ toolkit/components/resistfingerprinting/RFPHelper.sys.mjs
@@ -803,9 +803,17 @@ class _RFPHelper {
     return false;
   }
 
+  _onWindowDoubleClick(e) {
+    if (e.target.classList.contains("browserStack")) {
+      e.currentTarget.shrinkToLetterbox();
+    }
+  }
+
   _attachWindow(aWindow) {
     this._fixRounding(aWindow);
     aWindow.addEventListener("sizemodechange", windowResizeHandler);
+    aWindow.shrinkToLetterbox = this.shrinkToLetterbox;
+    aWindow.addEventListener("dblclick", this._onWindowDoubleClick);
     aWindow.gBrowser.addTabsProgressListener(this);
     aWindow.addEventListener("TabOpen", this);
     const resizeObserver = (aWindow._rfpResizeObserver =
@@ -837,6 +845,8 @@ class _RFPHelper {
       let browser = tab.linkedBrowser;
       this._resetContentSize(browser);
     }
+    aWindow.removeEventListener("dblclick", this._onWindowDoubleClick);
+    delete aWindow.shrinkToLetterbox;
     aWindow.removeEventListener("sizemodechange", windowResizeHandler);
   }
 
