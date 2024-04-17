fee38504e9ef70e90e07cf2ac2cb5fa80d49f8e2
hackademix <giorgio@maone.net>
2024-04-17 18:32:13 +0200

    fixup! Bug 41918: Option to reuse last window size when letterboxing is enabled.
    Bug 42500: Fix restored window size after startup maximization

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1581,6 +1581,13 @@ BrowserGlue.prototype = {
       return;
     }
 
+    // We don't want to mess up with RFP new window / letterboxing machinery.
+    if (
+      Services.prefs.getBoolPref("privacy.resistFingerprinting", false)
+    ) {
+      return;
+    }
+
     let store = Services.xulStore;
     let getValue = attr =>
       store.getValue(AppConstants.BROWSER_CHROME_URL, "main-window", attr);
