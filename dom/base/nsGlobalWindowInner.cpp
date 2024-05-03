dd4c0979a49118049ff66e26899128de91ea850a
Fatih <fkilic@mozilla.com>
2024-05-03 17:28:37 +0000

    Bug 1830629: Remove unnecessary checks for pdf.js for RFP. r=tjr
    resource:// is exempt from RFP, so we no longer need to check pdf.js specifically.
    
    Differential Revision: https://phabricator.services.mozilla.com/D209359

--- dom/base/nsGlobalWindowInner.cpp
+++ dom/base/nsGlobalWindowInner.cpp
@@ -3601,20 +3601,9 @@ double nsGlobalWindowInner::GetDevicePixelRatio(CallerType aCallerType,
     return 1.0;
   }
 
-  if (nsIGlobalObject::ShouldResistFingerprinting(aCallerType,
-                                                  RFPTarget::Unknown)) {
-    // Spoofing the DevicePixelRatio causes blurriness in some situations
-    // on HiDPI displays. pdf.js is a non-system caller; but it can't
-    // expose the fingerprintable information, so we can safely disable
-    // spoofing in this situation. It doesn't address the issue for
-    // web-rendered content (including pdf.js instances on the web.)
-    // In the future we hope to have a better solution to fix all HiDPI
-    // blurriness...
-    nsAutoCString origin;
-    nsresult rv = this->GetPrincipal()->GetOrigin(origin);
-    if (NS_FAILED(rv) || origin != "resource://pdf.js"_ns) {
-      return 1.0;
-    }
+  if (nsIGlobalObject::ShouldResistFingerprinting(
+          aCallerType, RFPTarget::Unknown)) {
+    return 2.0;
   }
 
   if (aCallerType == CallerType::NonSystem) {
