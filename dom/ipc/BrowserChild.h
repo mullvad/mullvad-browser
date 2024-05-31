b0ad496c5eea5879cd6d8143394cc4090db3654b
Edgar Chen <echen@mozilla.com>
2024-05-31 11:13:46 +0000

    Bug 1743329 - Release pointer lock when xul popup is open; r=smaug,pbz
    Differential Revision: https://phabricator.services.mozilla.com/D211620

--- dom/ipc/BrowserChild.h
+++ dom/ipc/BrowserChild.h
@@ -696,6 +696,8 @@ class BrowserChild final : public nsMessageManagerScriptExecutor,
 
   mozilla::ipc::IPCResult RecvReleaseAllPointerCapture();
 
+  mozilla::ipc::IPCResult RecvReleasePointerLock();
+
  private:
   void HandleDoubleTap(const CSSPoint& aPoint, const Modifiers& aModifiers,
                        const ScrollableLayerGuid& aGuid);
