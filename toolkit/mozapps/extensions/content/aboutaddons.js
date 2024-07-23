dabcd40e523c275ed9ad526937284ff51288d126
Beatriz Rizental <bea@torproject.org>
2024-07-23 15:03:23 +0200

    fixup! Bug 41834: Hide "Can't Be Removed - learn more" menu line for uninstallable add-ons

--- toolkit/mozapps/extensions/content/aboutaddons.js
+++ toolkit/mozapps/extensions/content/aboutaddons.js
@@ -1532,6 +1532,8 @@ class AddonOptions extends HTMLElement {
           el.disabled = false;
           el.hidden = false;
           document.l10n.setAttributes(el, "remove-addon-button");
+          // eslint-disable-next-line max-len
+          // eslint-disable-next-line no-constant-condition, no-constant-binary-expression
         } else if (true || addon.isBuiltin) {
           // Just hide the uninstall button for any "special" add-on
           // which can't be removed (tor-browser#41834)
