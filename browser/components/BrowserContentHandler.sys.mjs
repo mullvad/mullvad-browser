e4de0ac82162904a6f30479841147b8817ed3e7c
Henry Wilkes <henry@torproject.org>
2024-08-28 15:03:52 +0100

    fixup! Bug 41568: Disable LaterRun
    Bug 42630: Revert patch.

--- browser/components/BrowserContentHandler.sys.mjs
+++ browser/components/BrowserContentHandler.sys.mjs
@@ -774,8 +774,8 @@ nsBrowserContentHandler.prototype = {
             additionalPage = Services.urlFormatter.formatURLPref(
               "startup.homepage_welcome_url.additional"
             );
-            // Disable 'later run' pages for new profiles (tor-browser#41568)
-            lazy.LaterRun.selfDestruct();
+            // Turn on 'later run' pages for new profiles.
+            lazy.LaterRun.enable(lazy.LaterRun.ENABLE_REASON_NEW_PROFILE);
             break;
           case OVERRIDE_NEW_MSTONE: {
             // Check whether we will restore a session. If we will, we assume
