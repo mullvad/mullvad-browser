7b928e4064a5474b6fc027b259c18077448bd85e
hackademix <giorgio@maone.net>
2024-10-16 23:30:49 +0200

    Bug 43101: Deep link to the startup security warning explanation.

--- security/sandbox/common/SandboxUtils.sys.mjs
+++ security/sandbox/common/SandboxUtils.sys.mjs
@@ -37,7 +37,7 @@ export var SandboxUtils = {
 
       let buttons = [
         {
-          supportPage: "install-firefox-linux",
+          supportPage: "install-firefox-linux#w_security-features-warning",
           "l10n-id": "sandbox-unprivileged-namespaces-howtofix",
         },
         {
