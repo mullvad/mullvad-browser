Kevinisagirl21/tor-browser

Bug 41149: Re-enable DLL injection protection in all builds not just nightlies

toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
@@ -531,7 +531,7 @@ continue_loading:
   return ret;
 }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER)
 // Map of specific thread proc addresses we should block. In particular,
 // LoadLibrary* APIs which indicate DLL injection
 static void* gStartAddressesToBlock[4];
@@ -544,7 +544,7 @@ static bool ShouldBlockThread(void* aStartAddress) {
     return false;
   }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER)
   for (auto p : gStartAddressesToBlock) {
     if (p == aStartAddress) {
       return true;
@@ -618,7 +618,7 @@ MFBT_API void DllBlocklist_Initialize(uint32_t aInitFlags) {
     }
   }
 
-#if defined(NIGHTLY_BUILD)
+#if defined(NIGHTLY_BUILD) || defined(BASE_BROWSER)
   // Populate a list of thread start addresses to block.
   HMODULE hKernel = GetModuleHandleW(L"kernel32.dll");
   if (hKernel) {

mullvad/mullvad-browser

toolkit/xre/dllservices/mozglue/WindowsDllBlocklist.cpp
@@ -517,7 +517,7 @@ continue_loading:
   return ret;
 }
 
-#if defined(BLOCK_LOADLIBRARY_INJECTION)
+#if defined(BLOCK_LOADLIBRARY_INJECTION) || defined(BASE_BROWSER_VERSION)
 // Map of specific thread proc addresses we should block. In particular,
 // LoadLibrary* APIs which indicate DLL injection
 static void* gStartAddressesToBlock[4];
@@ -530,7 +530,7 @@ static bool ShouldBlockThread(void* aStartAddress) {
     return false;
   }
 
-#if defined(BLOCK_LOADLIBRARY_INJECTION)
+#if defined(BLOCK_LOADLIBRARY_INJECTION) || defined(BASE_BROWSER_VERSION)
   for (auto p : gStartAddressesToBlock) {
     if (p == aStartAddress) {
       return true;
@@ -593,7 +593,7 @@ MFBT_API void DllBlocklist_Initialize(uint32_t aInitFlags) {
     }
   }
 
-#if defined(BLOCK_LOADLIBRARY_INJECTION)
+#if defined(BLOCK_LOADLIBRARY_INJECTION) || defined(BASE_BROWSER_VERSION)
   // Populate a list of thread start addresses to block.
   HMODULE hKernel = GetModuleHandleW(L"kernel32.dll");
   if (hKernel) {