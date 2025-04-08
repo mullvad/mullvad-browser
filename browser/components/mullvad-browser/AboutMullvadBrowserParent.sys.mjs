167fc8d81caf3d703ae9422c6bf77bc0ce66f11d
Henry Wilkes <henry@torproject.org>
2025-04-08 19:06:32 +0100

    fixup! MB 39: Add home page about:mullvad-browser
    MB 411: Use the same update URL in about:mullvad-browser as the about
    dialog.

--- browser/components/mullvad-browser/AboutMullvadBrowserParent.sys.mjs
+++ browser/components/mullvad-browser/AboutMullvadBrowserParent.sys.mjs
@@ -5,21 +5,28 @@ export class AboutMullvadBrowserParent extends JSWindowActorParent {
   receiveMessage(message) {
     const shouldNotifyPref = "mullvadbrowser.post_update.shouldNotify";
     switch (message.name) {
-      case "AboutMullvadBrowser:GetUpdateData":
+      case "AboutMullvadBrowser:GetUpdateData": {
         if (!Services.prefs.getBoolPref(shouldNotifyPref, false)) {
           return Promise.resolve(null);
         }
         Services.prefs.clearUserPref(shouldNotifyPref);
+        // Try use the same URL as the about dialog. See mullvad-browser#411.
+        let updateURL = Services.urlFormatter.formatURLPref(
+          "app.releaseNotesURL.aboutDialog"
+        );
+        if (updateURL === "about:blank") {
+          updateURL = Services.urlFormatter.formatURLPref(
+            "startup.homepage_override_url"
+          );
+        }
+
         return Promise.resolve({
           version: Services.prefs.getCharPref(
             "browser.startup.homepage_override.mullvadbrowser.version"
           ),
-          url:
-            Services.prefs.getCharPref("mullvadbrowser.post_update.url", "") ||
-            Services.urlFormatter.formatURLPref(
-              "startup.homepage_override_url"
-            ),
+          url: updateURL,
         });
+      }
     }
     return undefined;
   }
