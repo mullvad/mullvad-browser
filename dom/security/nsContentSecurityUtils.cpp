85f273f402ed252d58cdb466325a8657037ce80d
Cathy Lu <calu@mozilla.com>
2025-06-05 21:42:26 +0000

    Bug 1791322 - GeckoView should call classifyDownloads to sandbox downloads r=geckoview-reviewers,nika
    Differential Revision: https://phabricator.services.mozilla.com/D249683

--- dom/security/nsContentSecurityUtils.cpp
+++ dom/security/nsContentSecurityUtils.cpp
@@ -2206,8 +2206,7 @@ void nsContentSecurityUtils::LogMessageToConsole(nsIHttpChannel* aChannel,
 }
 
 /* static */
-long nsContentSecurityUtils::ClassifyDownload(
-    nsIChannel* aChannel, const nsAutoCString& aMimeTypeGuess) {
+long nsContentSecurityUtils::ClassifyDownload(nsIChannel* aChannel) {
   MOZ_ASSERT(aChannel, "IsDownloadAllowed without channel?");
 
   nsCOMPtr<nsILoadInfo> loadInfo = aChannel->LoadInfo();
