3f83d92bcd4e432c6af019adf093f245c924e943
Henry Wilkes <henry@torproject.org>
2025-07-09 11:10:25 +0100

    fixup! BB 31575: Disable Firefox Home (Activity Stream)
    TB 43886: Drop base-browser changes to AboutNewTabRedirector.sys.mjs
    since the module is now excluded.

--- browser/components/newtab/AboutNewTabRedirector.sys.mjs
+++ browser/components/newtab/AboutNewTabRedirector.sys.mjs
@@ -416,7 +416,20 @@ class BaseAboutNewTabRedirector {
    * the newtab page has no effect on the result of this function.
    */
   get defaultURL() {
-    return "about:blank";
+    // Generate the desired activity stream resource depending on state, e.g.,
+    // "resource://newtab/prerendered/activity-stream.html"
+    // "resource://newtab/prerendered/activity-stream-debug.html"
+    // "resource://newtab/prerendered/activity-stream-noscripts.html"
+    return [
+      "resource://newtab/prerendered/",
+      "activity-stream",
+      // Debug version loads dev scripts but noscripts separately loads scripts
+      this.activityStreamDebug && !this.privilegedAboutProcessEnabled
+        ? "-debug"
+        : "",
+      this.privilegedAboutProcessEnabled ? "-noscripts" : "",
+      ".html",
+    ].join("");
   }
 
   newChannel() {
