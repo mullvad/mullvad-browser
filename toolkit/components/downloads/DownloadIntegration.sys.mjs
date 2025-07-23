aec66f8d709ca01f10c00960f96261b7aca094b4
Greg Stoll <gstoll@mozilla.com>
2025-07-23 17:30:24 +0000

    Bug 1972764 - make Content Analysis work if the download's redirects are non-null  a=diannaS
    I was wrong about the interface to download.saver.getRedirects(), and
    this is the correct way to iterate over them.
    
    Original Revision: https://phabricator.services.mozilla.com/D254279
    
    Differential Revision: https://phabricator.services.mozilla.com/D258211

--- toolkit/components/downloads/DownloadIntegration.sys.mjs
+++ toolkit/components/downloads/DownloadIntegration.sys.mjs
@@ -455,7 +455,7 @@ export var DownloadIntegration = {
 
     let redirects = download.saver.getRedirects();
     if (redirects) {
-      for (let redirect of redirects) {
+      for (let redirect of redirects.enumerate()) {
         resources.push({
           url: redirect.referrerURI,
           type: Ci.nsIClientDownloadResource.DOWNLOAD_REDIRECT,
