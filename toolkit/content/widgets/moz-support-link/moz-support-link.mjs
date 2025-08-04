1ccb48e40ca5699f8baf9ff16986d2503d1df4c8
Henry Wilkes <henry@torproject.org>
2025-08-04 16:10:20 +0100

    fixup! BB 42583: Modify moz-support-link for Base Browser.
    TB 43864: Move moz-support-link "preferences" patch to base-browser.

--- toolkit/content/widgets/moz-support-link/moz-support-link.mjs
+++ toolkit/content/widgets/moz-support-link/moz-support-link.mjs
@@ -125,6 +125,22 @@ export default class MozSupportLink extends HTMLAnchorElement {
       return;
     }
     let supportPage = this.getAttribute("support-page") ?? "";
+    // For base-browser we sometimes want to override firefox support links with
+    // our own.
+    // See tor-browser#40899.
+    switch (supportPage) {
+      case "preferences":
+        // Shown twice in preferences, both as `{ -brand-short-name } Support`.
+        // Instead of directing to support for preferences, we link to general
+        // tor browser support.
+        // See tor-browser#32092.
+        this.href = Services.prefs.getStringPref(
+          "browser.base-browser-support-url",
+          ""
+        );
+        return;
+      // Fall through to support.mozilla.org
+    }
     let base = MozSupportLink.SUPPORT_URL + supportPage;
     this.href = this.hasAttribute("utm-content")
       ? formatUTMParams(this.getAttribute("utm-content"), base)
